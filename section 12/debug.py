5881eace88c00d29cac6ec374cf57721b8ad0f90
Adrian Posadas <posadas.adrian20@gmail.com>
2023-04-25 15:33:34 -0700

    finished section 12

---
+++ section 12/debug.py
@@ -0,0 +1,19 @@
+# debugging
+
+# linting
+# num + 4
+
+# IDE / editor
+
+# read errors
+# 4 + 'dsaffasdf'
+
+# pdb - python debugger
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    t = 4 * 5
+    return num1 + num2
+
+add(4, 'asdf')
